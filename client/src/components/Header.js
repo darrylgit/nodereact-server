e0bed04fb0787ec5c2309bff95cffcb12e1d5a54
darrylgit <darrylsmithup@gmail.com>
2019-12-29 23:22:02 -0500

    Add and style header

---
+++ client/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a href="/" className="left brand-logo">
+            Emaily
+          </a>
+          <ul className="right">
+            <li>
+              <a href="/auth/google">Login With Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Header;
