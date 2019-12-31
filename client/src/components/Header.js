5af2854981451ed3b6e078c14282c76ee56212a0
darrylgit <darrylsmithup@gmail.com>
2019-12-30 22:13:07 -0500

    Toggle header content based on login status

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
 
 class Header extends React.Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
@@ -8,15 +28,15 @@ class Header extends React.Component {
           <a href="/" className="left brand-logo">
             Emaily
           </a>
-          <ul className="right">
-            <li>
-              <a href="/auth/google">Login With Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
