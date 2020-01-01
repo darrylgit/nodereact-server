da862131639f51d75941012b59d884c45530e06e
darrylgit <darrylsmithup@gmail.com>
2020-01-01 15:00:55 -0500

    Set up Payments componenet with React Stripe Checkout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import Payments from "./Payments";
 
 class Header extends React.Component {
   renderContent() {
@@ -15,9 +16,14 @@ class Header extends React.Component {
         );
       default:
         return (
-          <li>
-            <a href="/api/logout">Logout</a>
-          </li>
+          <>
+            <li>
+              <Payments />
+            </li>
+            <li>
+              <a href="/api/logout">Logout</a>
+            </li>
+          </>
         );
     }
   }
