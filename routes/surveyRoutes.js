986af7d471b7f3922b665d3a2ef93c11360f3151
darrylgit <darrylsmithup@gmail.com>
2020-01-07 10:19:30 -0500

    Send updated user in response after sending survey email

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,7 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
@@ -21,7 +21,16 @@ module.exports = app => {
 
     console.log(survey);
 
-    const mailer = new Mailer(survey, surveyTemplate(survey));
-    mailer.send();
+    try {
+      const mailer = new Mailer(survey, surveyTemplate(survey));
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 };
