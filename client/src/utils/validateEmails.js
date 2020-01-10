85e7a210138d43daea6b91d9d3687c6ccdc7a373
darrylgit <darrylsmithup@gmail.com>
2020-01-09 19:04:13 -0500

    Fix trailing comma bug in recipient list input

--- client/src/utils/validateEmails.js
+++ client/src/utils/validateEmails.js
@@ -1,10 +1,13 @@
 const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export default emails => {
-  const invalidEmails = emails
-    .split(",")
-    .map(email => email.trim())
-    .filter(email => re.test(email) === false);
+  let emailsArray = emails.split(",").map(email => email.trim());
+
+  if (emailsArray.slice(-1)[0] === "") {
+    emailsArray.pop();
+  }
+
+  const invalidEmails = emailsArray.filter(email => re.test(email) === false);
 
   if (invalidEmails.length) {
     return `These emails are invalid: ${invalidEmails}`;
