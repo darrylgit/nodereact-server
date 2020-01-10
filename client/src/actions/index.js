88935389e0de02939e9e5a9400a3fb92cd7a6d18
darrylgit <darrylsmithup@gmail.com>
2020-01-10 15:08:37 -0500

    Scaffold Send Survey button

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { FETCH_USER } from "./types";
+import { FETCH_USER, SUBMIT_SURVEY } from "./types";
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get("/api/current_user");
@@ -12,4 +12,10 @@ export const handleToken = token => async dispatch => {
   const res = await axios.post("/api/stripe", token);
 
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const submitSurvey = values => {
+  return {
+    type: SUBMIT_SURVEY
+  };
 };
--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
 import formFields from "./formFields";
+import * as actions from "../../actions";
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
   const reviewFields = formFields.map(({ name, label }) => {
     return (
       <div key={name}>
@@ -16,8 +17,18 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
     <div>
       <h5>Please confirm your entries</h5>
       {reviewFields}
-      <button className="yellow darken-3 btn-flat" onClick={onCancel}>
+      <button
+        className="yellow white-text darken-3 btn-flat"
+        onClick={onCancel}
+      >
         Back
+      </button>
+      <button
+        onClick={() => submitSurvey(formValues)}
+        className="green white-text btn-flat right"
+      >
+        Send Survey
+        <i className="material-icons right">email</i>
       </button>
     </div>
   );
@@ -27,4 +38,4 @@ function mapStateToProps(state) {
   return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(SurveyFormReview);
